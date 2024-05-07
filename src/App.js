df50dccf50f45c6414c1649278833fe51e7ab093
David Oranchak <doranchak@gmail.com>
2024-05-06 21:19:44 -0400

    highlights support

--- src/App.js
+++ src/App.js
@@ -16,8 +16,12 @@ function App() {
   // start
   const [start, setStart] = useState(-40);
   // grid
+  let gh = createGridHighlight(plaintext, k, n, start);
+  // gh[1][13] = 1;
+  // gh[3][2] = 1;
+  // gh[5][9] = 1;
+  const [gridHighlight, setGridHighlight] = useState(gh);  
   const [grid, setGrid] = useState(createGrid(plaintext, k, n, start));
-  const [gridHighlight, setGridHighlight] = useState(createGridHighlight(plaintext, k, n, start));  
 
   const plaintextChange = (event) => {
     let val = event.target.value;
@@ -44,35 +48,60 @@ function App() {
   };
   function handleSubmit() {
   }
+
+  function updateGrid(plaintext, k, n, start, highlights) {
+    // let gh = createGridHighlight(plaintext, k, n, start);
+    let gh = createGridHighlight(plaintext, k, n, start);
+    console.log(gh);
+    if (highlights) {
+      highlights.map((h, index) => {
+        console.log(h);
+        gh[h[0]][h[1]+1] = 1;
+      });
+    }
+    setGridHighlight(gh);
+    setPlaintext(plaintext);
+    setK(k);
+    setN(n);
+    setStart(start);
+    setGrid(createGrid(plaintext, k, n, start));
+  }
+
   function selectKaczynski1() {
-    setPlaintext("KACZYNSKI");
-    setK(10);
-    setN(7);
-    setStart(-30);
-    setGrid(createGrid("KACZYNSKI", 10, 7, -30));
+    updateGrid("KACZYNSKI", 10, 7, -30);
   }
   function selectKaczynski2() {
-    setPlaintext("THEODOREJKACZYNSKI");
-    setK(10);
-    setN(9);
-    setStart(-30);
-    setGrid(createGrid("THEODOREJKACZYNSKI", 10, 9, -30));
+    updateGrid("KACZYNSKI", 10, 7, -30, [[6,0],[1,1],[5,2],[0,3],[2,4],[3,5],[4,6]]);
   }
-  function selectTrees() {
-    setPlaintext("TREESTOBLOOMINWEEKS");
-    setK(10);
-    setN(6);
-    setStart(-40);
-    setGrid(createGrid("TREESTOBLOOMINWEEKS", 10, 6, -40));
+  function selectKaczynski3() {
+    updateGrid("KACZYNSKI", 10, 7, -30, [[3,3],[4,4],[4,5],[4,6],[4,7],[1,8]]);
   }
+  function selectKaczynski4() {
+    updateGrid("KACZYNSKI", 10, 7, -30, [[3,3],[4,4],[4,5],[4,6],[2,7],[2,8]]);
+  }
+  function selectTheodoreKaczynski1() {
+    updateGrid("THEODOREJKACZYNSKI", 10, 9, -30);
+  }
+  function selectTheodoreKaczynski2() {
+    updateGrid("THEODOREJKACZYNSKI", 10, 9, -30, [[4,1],[4,2],[6,3],[0,4],[6,5],[5,6],[4,8],[5,12],[6,13],[6,14],[6,15],[6,16],[3,17]]);
+  }
+  function selectTrees1() {
+    updateGrid("TREESTOBLOOMINWEEKS", 10, 6, -40);
+  }
+  function selectTrees2() {
+    updateGrid("TREESTOBLOOMINWEEKS", 10, 6, -40, [[1,0],[2,1],[1,2],[0,3],[0,5],[1,6],[2,7],[2,9],[4,10],[5,10],[2,11],[2,12],[1,13],[4,14],[1,17],[2,18]]);
+  }
+
   function cl(row, col, highlight) {
-    // let cl = highlight ? "col highlight" : "col";
     let shift = (n-1-row)*k + start;
-    if (shift == 0) return "plaintext col";
-    return "col";
+    let c = "col";
+    if (shift == 0) c += " plaintext";
+    if (highlight) 
+      if (shift == 0) c += " highlight1";
+      else c += " highlight2"
+    return c;
   }
-  
-    
+   
   return (
     <>
       <center>
@@ -81,9 +110,14 @@ function App() {
             <tbody>
               <tr>
                 <td>
-                  <a href="#" onClick={selectKaczynski1}>Kaczynski1</a> |&nbsp;
-                  <a href="#" onClick={selectKaczynski2}>Kaczynski2</a> |&nbsp;
-                  <a href="#" onClick={selectTrees}>Trees</a>
+                  Kaczynski <a href="#" onClick={selectKaczynski1}>[1]</a>
+                  <a href="#" onClick={selectKaczynski2}>[2]</a>
+                  <a href="#" onClick={selectKaczynski3}>[3]</a>
+                  <a href="#" onClick={selectKaczynski4}>[4]</a><br></br>
+                  TheodoreKaczynski <a href="#" onClick={selectTheodoreKaczynski1}>[1]</a>
+                  <a href="#" onClick={selectTheodoreKaczynski2}>[2]</a><br></br>
+                  Trees <a href="#" onClick={selectTrees1}>[1]</a>
+                  <a href="#" onClick={selectTrees2}>[2]</a>
                 </td>
                 <td>
                 <table>
@@ -148,7 +182,7 @@ function App() {
                   {grid.map((row, rowIndex) => (
                     <div className="row" key={rowIndex}>
                       {row.map((col, colIndex) => (
-                        <div style={style(rowIndex, colIndex, 0)} className={cl(rowIndex, colIndex, grid[rowIndex][colIndex])} key={`${rowIndex}-${colIndex}`}>{grid[rowIndex][colIndex]}</div>
+                        <div style={style(rowIndex, colIndex, 0)} className={cl(rowIndex, colIndex, gridHighlight[rowIndex][colIndex])} key={`${rowIndex}-${colIndex}`}>{grid[rowIndex][colIndex]}</div>
                       ))}
                     </div>
                   ))}
@@ -193,7 +227,7 @@ function createGridHighlight(plaintext, k, n, start) {
   let cols = plaintext.length + 1;  
   for (let i = rows-1; i >= 0; i--) {
     const row = [];
-    for (let j = 0; j < cols; j++) {
+    for (let j = 0; j <= cols; j++) {
       row.push(0);
     }
     gridHighlight.push(row);
